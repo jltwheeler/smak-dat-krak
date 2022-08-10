af8c896678c5895aeaf6d03f663d86c7a4c6d4a9
Josh Wheeler <jltwheeler@gmail.com>
2022-08-10 21:55:57 +0100

    chore: initial project boilerplate yo

---
+++ esbuild.config.js
@@ -0,0 +1,16 @@
+const esbuild = require("esbuild");
+
+esbuild
+  .build({
+    entryPoints: ["./src/index.ts"],
+    bundle: true,
+    minify: true,
+    platform: "node",
+    sourcemap: true,
+    target: "node16",
+    external: ["aws-sdk"],
+    define: { "require.resolve": undefined },
+    treeShaking: true,
+    outdir: "dist",
+  })
+  .catch(() => process.exit(1));
