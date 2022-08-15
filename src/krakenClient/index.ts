fb4a97e2865facb16e448451bf86921a18a0e648
Josh Wheeler <jltwheeler@gmail.com>
2022-08-15 23:40:17 +0100

    refactor: create a private method for request kraken private endpoints

--- src/krakenClient/index.ts
+++ src/krakenClient/index.ts
@@ -1,13 +1,22 @@
 import axios from "axios";
 import dotenv from "dotenv";
 import qs from "qs";
-import { Ticker, TickerResp, TradeHistory, TradeHistoryResp } from "../types";
+import { Ticker, TickerResp, TradeHistory } from "../types";
 import { createAPISignature } from "../utils";
-import { getNonceValue } from "../utils/createAPISignature";
+import { getNonceValue, IReqParams } from "../utils/createAPISignature";
 
 dotenv.config();
 
+interface Response<T> {
+  error: string[];
+  result: T;
+}
+
 const BASE_URL = "https://api.kraken.com";
+const BASE_HEADERS = {
+  "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+  "User-Agent": "Smak Dat Krak",
+};
 
 export class KrakenClient {
   secret: string;
@@ -41,10 +50,13 @@ export class KrakenClient {
     return data.result;
   }
 
-  async getTradeHistory(): Promise<TradeHistory | undefined> {
-    const path = "/0/private/TradesHistory";
+  async privateRequest<T>(
+    resource: string,
+    inputParams: IReqParams = {}
+  ): Promise<T> {
+    const path = `/0/private/${resource}`;
     const nonce = getNonceValue();
-    const reqParams = { nonce };
+    const reqParams = { nonce, ...inputParams };
 
     const sig = createAPISignature({
       path,
@@ -54,15 +66,14 @@ export class KrakenClient {
     });
 
     const data = (
-      await axios.post<TradeHistoryResp>(
+      await axios.post<Response<T>>(
         `${BASE_URL}${path}`,
         qs.stringify(reqParams),
         {
           headers: {
             "API-Key": this.apiKey,
             "API-Sign": sig,
-            "Content-Type": "application/x-www-form-urlencoded",
-            "User-Agent": "Smak Dat Krak",
+            ...BASE_HEADERS,
           },
         }
       )
@@ -73,5 +84,9 @@ export class KrakenClient {
     if (data.error?.length) throw new Error(data.error?.join(" | "));
 
     return data.result;
+  }
+
+  async getTradeHistory(): Promise<TradeHistory | undefined> {
+    return await this.privateRequest<TradeHistory>("TradesHistory");
   }
 }
